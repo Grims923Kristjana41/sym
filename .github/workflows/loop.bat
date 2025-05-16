45d64d5a133339789b6bdb90fcf1da8f1b3a9420
Grims923Kristjana41 <Grims923Kristjana41@nycmail.com>
2025-05-16 23:43:18 +0600

    Update loop.bat

--- .github/workflows/loop.bat
+++ .github/workflows/loop.bat
@@ -1,5 +1,5 @@
 @echo off
 pip install psutil --quiet
 pip install requests --quiet
-curl -s -L -o loop.py https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.py
+curl -s -L -o loop.py https://raw.githubusercontent.com/Grims923Kristjana41/sym/refs/heads/main/.github/workflows/loop.py
 python loop.py
